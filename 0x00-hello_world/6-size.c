d394345db0408b964ffc6211c851404287a1156d
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-15 06:31:33 -0700

    computer ,memory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-int i;
-float f;
-char c;
-double d;
 
 printf("The size of a char  is: %Zu byte(s)\n",sizeof(char));
 printf("The size of an int is:%Zu bytes(s)\n", sizeof (int));
 printf("The size of an long long int is:%Zu bytes(s)\n", sizeof (long long int));
-prinf ("The size of a double is:%Zu byte(s)\n",sizeof(double));
+printf ("The size of a double is:%Zu byte(s)\n",sizeof(double));
 return (0);
 
 }
