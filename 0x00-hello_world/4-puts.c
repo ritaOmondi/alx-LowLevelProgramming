a44de65e9b51ce03e0eaf5984bfd95f02e018fc8
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-15 05:22:54 -0700

    Betty description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /*
- *Outputing "Programming is like building a multilingual puzzle
+ *Outputing "Programming is like building a multilingual puzzle"
  *Return 0,
  */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+
+puts ("\"Programming is like building a multilingual puzzle\n");
 return (0);
+
 }
