1984f1241d6a09015e8470176bbf9c49d03d2ed3
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-16 11:47:12 -0700

    numerics 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers to the base ten
+ * Return: Always  (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf ("%d", x);
+	printf ("\n");
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,4 +3,5 @@
 2. Listing alphabetical numbers in lowercase.
 3. Listing alphabets in lowercase then Uppercase.
 4. Listing all alphabets except e and q.
-5.    
+5.  Listing digits from 0 to 10.
+6.     
