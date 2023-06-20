176e9123ec8d7d2a9ee27d2ee049605adb6a2d1b
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-20 07:35:41 -0700

    alpha alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabetic characters
+ * @c - character to be checked
+ * return 1 for alphabetic characters or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,6 @@
 0. print out  putchar.
 1. Printing alphabets in lowercase.
-2.  
+2. Printitng alphabets x10.
+3. Displaying characters that are in lower case.
+4. Displaying alpha characters
+5.  
