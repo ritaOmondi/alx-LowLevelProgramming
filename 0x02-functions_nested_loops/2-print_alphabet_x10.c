6b2c2a89932f6ac39fbaf2f6bb15917d29be492f
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-20 06:12:13 -0700

    printing alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_alphabets_x10
+ * return void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char x;
+	int count=0;
+
+	while (count <= 9)
+	{
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+	count++;
+	
+	}
+
+
+
+
+}
