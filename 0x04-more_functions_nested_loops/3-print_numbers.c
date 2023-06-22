07edc237462fdccf9a917d7f7e6ed45d9356c5d3
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-22 02:10:45 -0700

    printing numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number: Prints numbers specifies\d by the condition
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+	_putchar(j + '0');
+	_putchar('\n');
+	}
+
+
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,5 @@
 0. isupper(): checks whether the oassed character is uppercase letter.
-1. 
+1. isdigit(): returns a digit
+2. mul(): multplies integers.
+3. print_numbers: prints numbers from 0 to 9.
+4.  
