d0c62ed4bad566f83082628553182af3e907fea6
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-26 02:00:49 -0700

    pointer dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value to 98
+ *
+ * Return: Always Success
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+
+
+
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0. Pointer dereferencing.
+1. 
