77395756841bb6b170c09db7d1f49beeaed87cc9
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-26 04:25:51 -0700

    printing in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,8 +2,10 @@
 /**
  * print_rev - prints a string in reverse followed by a new line
  * @s - pointer string to be reversed
- * Description : The function prints the character of strings
- * in reverse then returns a new line
+ * Description : The function takes a pointer to a string as a parameter
+ * and prints the character of string in reverse order to the standard 
+ * output. Starts at the end  of the line to the beginning of the
+ * string 
  */
 void print_rev(char *s)
 {
