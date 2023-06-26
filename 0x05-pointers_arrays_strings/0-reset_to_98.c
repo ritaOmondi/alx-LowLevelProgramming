6ef838e079e99df8a58ad7272b5e063330091a64
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-26 02:08:47 -0700

    dereferencing pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value to 98
+ * reset_to_98 - updates the value pointed to to 98
+ *@n: pointer to the integer
  *
+ * Description: The function takes the pointer to an integer as a parameter
+ * and updates the value to 98
  * Return: Always Success
  */
 void reset_to_98(int *n)
