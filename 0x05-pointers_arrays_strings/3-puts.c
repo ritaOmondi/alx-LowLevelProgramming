b87526b8bb751f84b5d9f13ca17796367c1f12e2
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-26 03:58:40 -0700

    printing characters of a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,12 @@
 #include "main.h"
 /**
- * _puts - prints a string
- * *str : pointer string to be printed
+ * _puts - prints a string followed by a new line
+ * *str : pointer to the string
+ *
+ * Description : Function takes pointer to the string as a parameter
+ * and prints the character of the string to a standard output.
+ * It prints while looping through the characters untill null terminator is reached.
+ * It is followed by a new line
  * Return : void
  */
 
