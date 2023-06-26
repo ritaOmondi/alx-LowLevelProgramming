c6a9d4106fdc4242a65f3764e005c7861bb9032f
ritaOmondi <cherylritaomondi@gmail.com>
2023-06-26 07:47:34 -0700

    converting string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,16 @@
 #include "main.h"
  /**
   * _atoi : converts a string to an integer
-  * @s :pointer string to be convet=rted to integer
-  */
+  * * @s: Pointer to the string.
+ *
+ * Description: This function takes a pointer to a string as a parameter
+ * and converts the string to an integer. It skips any leading whitespace
+ * characters and then processes the characters until a non-digit character
+ * is encountered or the end of the string is reached. The resulting integer
+ * value is returned.
+ *
+ * Return: The converted integer value.
+ */
 int _atoi(char *s)
 {
 
@@ -15,9 +23,9 @@ int _atoi(char *s)
 	{
 	if (s[a] == 45)
 	{
-	mn *= - 1;
+	mn *= -1;
 	}
-	while (s[a] >= 48 && s[a] <= 57 )
+	while (s[a] >= 48 && s[a] <= 57)
 	{
 	stng = 1;
 	un = (un * 10) + (s[a] - '0');
