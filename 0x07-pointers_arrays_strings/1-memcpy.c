3f9afe251b09e3f1f7f6143ecc41c4d475fe78a5
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-03 04:12:59 -0700

    copying memeory of block from dest to src using function memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ * @dest : input
+ * @src : input
+ * @n : n
+ * Return : Always (0) sucess
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int a;
+	
+	for (a = 0; a < n; a++)
+	{
+	dest[a] = src[a];
+	}
+	return (dest);
+}
