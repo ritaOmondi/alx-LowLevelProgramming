95cc40a7ad583d115684c07db0a449bbd9358f1e
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-03 07:52:03 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string : sets value of a pointer to a char
+ *@s : double pointer
+ *to : character pointer
+ * Return : Success
+ */
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+}
+ 
