b964c63ffb1ac1894efcc94657ed781c75338bc0
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-07 03:58:08 -0700

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name
+ * @argc : argument count
+ * @argv : argument vector
+ * Return : Always
+ */
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+arguments
+0. int main(int argc, char argv[]):print the new name, without having to compile it again
+1. 
