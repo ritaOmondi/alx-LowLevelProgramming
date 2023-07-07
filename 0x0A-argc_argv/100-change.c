d7ed8f4acc9ee4980c375299a835e2d48974bdc5
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-07 06:44:01 -0700

    printing minimum change coin

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing the minimum coin change
+ * @argc : argument count
+ * @argv : argument vector
+ *Return : Always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coin = 0;
+
+	if (argc != 2)
+	{
+	printf("error\n");
+	return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+	coin++;
+	if ((cents - 25) >= 0)
+	{
+	cents -= 25;
+	continue;
+	}
+	if ((cents - 10) >= 0)
+	{
+	cents -= 10;
+	continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+	cents -= 5;
+	continue;
+	}
+	if ((cents - 2) >= 2)
+	{
+	cents -= 2;
+	continue;
+	}
+	cents--;
+	}
+	printf("%d\n", coin);
+	return (0);
+}
