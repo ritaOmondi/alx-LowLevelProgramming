7da1399b148959d5edcaebfc3c51236e74d6cdda
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-17 03:12:33 -0700

    strcuture struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - defines the variables of a dog
+ *@name : first member
+ *@age : second parameter
+ *@owner : third parameter
+ * Return : Always (0) success
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
