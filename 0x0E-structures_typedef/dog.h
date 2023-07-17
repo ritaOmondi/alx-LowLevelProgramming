b61cf6882baad2255d1264d4897c46f4f9b1bb49
ritaOmondi <cherylritaomondi@gmail.com>
2023-07-17 03:40:44 -0700

    initializing struct variables

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdio.h>
 /**
  * struct dog - defines the variables of a dog
@@ -15,4 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
