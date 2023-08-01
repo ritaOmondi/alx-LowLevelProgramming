e57f4b0d1c87b7cd467c0928d6147c1bbc27a7ee
ritaOmondi <cherylritaomondi@gmail.com>
2023-08-01 04:08:27 -0700

    print safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
  * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
@@ -8,23 +11,69 @@
  * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
-size_t print_listint_safe(const listint_t *head)
+size_t looped_listint_len(const listint_t *head)
 {
-const listint_t *slow = head;
-const listint_t *fast = head;
-size_t countN = 1;
+const listint_t *slow, *fast;
+size_t 	countN = 1;
 
-while (slow != NULL && fast != NULL && fast->next != NULL)
+if (head == NULL || head->next == NULL)
+return (0);
+slow= head->next;
+fast = (head->next)->next;
+while (fast)
 {
-printf("%d\n", slow->n);
-slow = slow->next;
-fast = fast->next->next;
 if (slow == fast)
 {
-printf("%d\n", slow->n);
-exit(98);
-}
+slow = head;
+while (slow != fast)
+{
 countN++;
+slow = slow->next;
+fast = fast->next;
+}
+slow = slow->next;
+while (slow != fast)
+{
+countN++;
+slow = slow->next;
 }
 return (countN);
 }
+slow = slow->next;
+fast = (fast->next)->next;
+}
+return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nodes, index = 0;
+
+nodes = looped_listint_len(head);
+if (nodes == 0)
+{
+for (; head != NULL; nodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (nodes);
+}
+
+
